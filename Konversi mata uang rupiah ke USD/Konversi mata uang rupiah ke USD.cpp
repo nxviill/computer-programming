9009c3d753a5a4ad1677a4fe5b0c7243334356ad
Nabila <89417991+nxviill@users.noreply.github.com>
2021-09-09 16:22:05 +0700

    Create Konversi mata uang rupiah ke USD.cpp
    Program konversi mata uang rupiah ke dolar dengan menggunakan input dari user yaitu yang berupa nominal mata uang rupiah, jika diketahui untuk 1 USD = Rp 9.600

---
+++ Konversi mata uang rupiah ke USD/Konversi mata uang rupiah ke USD.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float rupiah, dolar;
+	
+	cout<<"KONVERSI MATA UANG RUPIAH KE DOLLAR"<<endl;
+	cout<<"-----------------------------------"<<endl;
+	cout<<"Masukkan Rupiah : ";
+	cin>>rupiah;
+	dolar=rupiah/9600;
+	cout<<"Dolar = "<<dolar<<" USD";
+}
