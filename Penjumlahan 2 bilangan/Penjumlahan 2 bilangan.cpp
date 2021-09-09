e8afdd26744e8db73b0e2dd9f91d044741c81c4c
Nabila <89417991+nxviill@users.noreply.github.com>
2021-09-09 16:15:52 +0700

    Create Penjumlahan 2 bilangan.cpp
    Program penjumlahan 2 buah bilangan dengan menggunakan input (masukan) dari user (Pengguna)

---
+++ Penjumlahan 2 bilangan/Penjumlahan 2 bilangan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int nilai1, nilai2, jumlah;
+	//Input nilai 1
+	cout<<"Masukkan nilai 1: ";
+	cin >> nilai1;
+	//Input nilai 2
+	cout<<"Masukkan nilai 2: ";
+	cin >> nilai2;
+	cout<<"---------------------- +"<<endl;
+	//Proses penjumlahan
+	jumlah=nilai1+nilai2;
+	cout<<"Hasil: "<<jumlah<<endl<<endl;
+}
